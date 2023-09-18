b9c847acb0581a70a0a082956aaac5fa0f87ac3a
Ethan Skowronski <ethanski22@icloud.com>
2023-09-18 13:34:45 -0400

    added all of the character scripts to the proper character

--- Assets/Banana.cs
+++ Assets/Banana.cs
@@ -1,19 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Banana : MonoBehaviour
 {
+    public Keyboard keyboard;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        keyboard = Keyboard.current;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Use * Time.deltaTime to operate in seconds instead of frames
+
         // Displays a message to the console tab in Unity
-        Debug.Log("Banana");
+        // Debug.Log("Banana");
+
+        if (keyboard == null)
+        {
+            Debug.LogError("No keyboard found");
+        }
+
+        if (keyboard.wKey.isPressed)
+        {
+            Debug.Log("You pressed the W key");
+        }
     }
 }
