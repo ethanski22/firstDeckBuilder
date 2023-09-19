bae2c96516bc70b3b7bfe1e5e349a5f02e7b004f
Ethan Skowronski <ethanski22@icloud.com>
2023-09-19 18:48:55 -0400

    Changing player stats system

--- Assets/Character Scripts/BarbarianScript.cs
+++ Assets/Character Scripts/BarbarianScript.cs
@@ -9,9 +9,9 @@ public class BarbarianScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        PlayerPrefs.SetInt("HealthPoints", 100);
+        PlayerPrefs.SetInt("BarbarianHP", 100);
+        PlayerPrefs.SetInt("Money", 0);
         PlayerPrefs.Save();
-        PlayerPrefs.SetInt("H", 1);
     }
 
     // Update is called once per frame
@@ -19,43 +19,4 @@ public class BarbarianScript : MonoBehaviour
     {
         
     }
-
-    public class Character : CharacterStats
-    {
-        public Character(string characterName, 
-            string characterClass, 
-            int characterHealth, 
-            int characterDefence,
-            int characterWealth,
-            int characterPower,
-            int characterPoison,
-            bool isAlive,
-            bool isAwake,
-            int characterPosition) : 
-            base(characterName, 
-                characterClass, 
-                characterHealth, 
-                characterDefence,
-                characterWealth,
-                characterPower,
-                characterPoison,
-                isAlive,
-                isAwake,
-                characterPosition)
-        {
-            characterName = "Input Name";
-            characterClass = "Barbarian";
-            characterHealth = 100; // Change the health later to match each class
-            characterDefence = 0;
-            characterWealth = 100;
-            characterPower = 5; // Cards will have a certain amount of power to be able to play
-            characterPoison = 0;
-            isAlive = true;
-            isAwake = true;
-            characterPosition = 0;
-        }
-    }
-
-
-    
 }
--- Assets/PlayerStats.cs
+++ Assets/PlayerStats.cs
@@ -2,62 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PlayerStats", menuName = "My Game/Player Stats")]
-public class PlayerStats : ScriptableObject
+public class PlayerStats : MonoBehaviour
 {
-    public string characterName;
-    public string characterClass;
-    public int characterHealth;
-    public int characterDefence;
-    public int characterWealth;
-    public int characterPower;
-    public int characterPoison;
-    public bool isAlive;
-    public bool isAwake;
-    public int characterPosition;
-    public GameObject characterModel;
-}
-
-// This class is outside of the PlayerStats class because 
-// if it is in the class the code will get bitchy
-public class CharacterStats
-{
-    // Capital letters are known for public fields
-
-    // When we create a character a name is required
-    public string CharacterName { get; set; }
-    public string CharacterClass { get; set; }
-    public int CharacterHealth { get; set; }
-    public int CharacterDefence { get; set; }
-    public int CharacterWealth { get; set; }
-    public int CharacterPower { get; set; } // Amount of cards that player could play
-    public int CharacterPoison { get; set; } 
-    public bool IsAlive { get; set; }
-    public bool IsAwake { get; set; }
-    public int CharacterPosition { get; set; } // Use this to figure out where to put the character on the screen
-
-    // Doing this helps to not create conflicts in our code
-    public CharacterStats(string characterName, 
-        string characterClass, 
-        int characterHealth, 
-        int characterDefence,
-        int characterWealth,
-        int characterPower,
-        int characterPoison,
-        bool isAlive,
-        bool isAwake,
-        int characterPosition)
+    // Start is called before the first frame update
+    void Start()
     {
-        CharacterName = characterName;
-        CharacterClass = characterClass;
-        CharacterHealth = characterHealth;
-        CharacterDefence = characterDefence;
-        CharacterWealth = characterWealth;
-        CharacterPower = characterPower;
-        CharacterPoison = characterPoison;
-        IsAlive = isAlive;
-        IsAwake = isAwake;
-        CharacterPosition = characterPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
     }
 }
+
 
