b79ff3511ce15607e6ed1af56b3b69768323bc6a
Ethan Skowronski <ethanski22@icloud.com>
2023-09-21 10:45:56 -0400

    fixed semicolon error

--- Assets/PlayerStats.cs
+++ Assets/PlayerStats.cs
@@ -7,7 +7,7 @@ public class PlayerStats : MonoBehaviour
     void PlayerStatPrefs()
     {
         PlayerPrefs.SetString("PlayerName", "Player");
-        PlayerPrefs.SetInt("PlayerLevel", 0)
+        PlayerPrefs.SetInt("PlayerLevel", 0);
     }
 
     // Start is called before the first frame update
