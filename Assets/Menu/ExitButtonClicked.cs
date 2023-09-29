6f9b5bd5d53b1e8572db988007dacc3cc33e3b30
Ethan Skowronski <ethanski22@icloud.com>
2023-09-29 14:05:24 -0400

    did stuff

---
+++ Assets/Menu/ExitButtonClicked.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class ExitButtonClicked : MonoBehaviour
+{
+    public void ExitGameButton()
+    {
+
+    }
+}
