4099dd82848da5cc4841a8766cb72233b62007de
Ethan Skowronski <ethanski22@icloud.com>
2023-10-05 13:01:54 -0400

    added stuff to make visual studio better

--- Assets/Menu/MainMenuScript.cs
+++ Assets/Menu/MainMenuScript.cs
@@ -8,24 +8,33 @@ using TMPro;
 public class ExitButtonClicked : MonoBehaviour
 {
     [Header("Levels to Load")]
-    public string apple;
+    public string newLevel;
+    public string loadLevel;
     public string noLoadText;
     public Text noLoadElement;
+    [SerializeField] private GameObject noSavedGame = null;
 
+    // Runs when the New Game button is clicked
     public void NewGameClick()
     {
-        if (apple == null)
-        {
-            //LoadGameButton.enabled = false;
-        }
-        else
-        {
-            SceneManager.LoadScene(apple);
-        }
+        SceneManager.LoadScene(newLevel);
     }
 
     public void LoadGameClick()
     {
-        SceneManager.LoadScene(apple);
+        if (PlayerPrefs.HasKey("CurrentLocation"))
+        {
+            loadLevel = PlayerPrefs.GetString("CurrentLocation");
+            SceneManager.LoadScene(loadLevel);
+        }
+        else
+        {
+            noSavedGame.SetActive(true);
+        }
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
     }
 }
