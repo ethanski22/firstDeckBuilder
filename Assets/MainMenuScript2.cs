19064980d913e49a85b4691697851a142b51af18
Ethan Skowronski <ethanski22@icloud.com>
2023-10-11 13:21:28 -0400

    Midday save
    Still on audio effects

--- Assets/MainMenuScript2.cs
+++ Assets/MainMenuScript2.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenuScript2 : MonoBehaviour
 {
+
+    public void Start()
+    {
+        if (PlayerPrefs.HasKey("mainVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetEffectsVolume();
+        }
+
+    }
+
+
     [Header("Load game settings")]
     public string loadLevel;
     public string newGameLevel;    
@@ -13,9 +30,10 @@ public class MainMenuScript2 : MonoBehaviour
     [SerializeField] private GameObject gameText = null;
 
     [Header("Volume settings")]
-    [SerializeField] private Text mainVolumeText = null;
-    [SerializeField] private Text musicVolumeText = null;
-    [SerializeField] private Text effectsVolumeText = null;
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private GameObject mainVolumeText = null;
+    [SerializeField] private GameObject musicVolumeText = null;
+    [SerializeField] private GameObject effectsVolumeText = null;
     [SerializeField] private Slider mainVolumeSlider = null;
     [SerializeField] private Slider musicVolumeSlider = null;
     [SerializeField] private Slider effectsVolumeSlider = null;
@@ -59,13 +77,25 @@ public class MainMenuScript2 : MonoBehaviour
     {
         AudioListener.volume = volume;
     }
-    public void SetMusicVolume(int volume)
+    public void SetMusicVolume()
     {
-        
+        float volume = musicVolumeSlider.value;
+        myMixer.SetFloat("music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
-    public void SetEffectsVolume(int volume)
+    public void SetEffectsVolume()
     {
-        
+        float volume = effectsVolumeSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
     }
 
+    public void LoadVolume()
+    {
+        musicVolumeSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        effectsVolumeSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+
+        SetMusicVolume();
+        SetEffectsVolume();
+    }
 }
